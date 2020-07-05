aa14edd1b65f277ef007815871edd3c7654a121d
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-05 18:31:04 -0400

    Update ai1.py

--- ai1.py
+++ ai1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import errno
 
@@ -10,19 +12,19 @@ except OSError as oe:
 		raise
 
 
-print("Opening FIFO1...")
 while ( True ):
     with open(FIFO1, 'r') as fifo1:
-        print("FIFO1 opened")
+        print("AI1: FIFO1 opened")
         while True:
             data = fifo1.read()
             if len(data) == 0:
-                print("Writer closed")
+                print("AI1: Writer closed")
                 break
-            print('Read: "{0}"'.format(data))
+            print('AI1: Read: "{0}"'.format(data))
 
 
     with open(FIFO1, 'w') as fifo1:
-        print("FIFO1 opened")
+        print("AI1: FIFO1 opened")
         fifo1.write("My name is Ai1")
-        print("Message sent")
+        print("AI1: Message sent")
+print("Never got here")
