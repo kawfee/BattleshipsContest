c7b1aa8cbf3f8f8d58f04ab00901362a01d6dd6a
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-06 15:01:45 -0400

    Update otherTest.py

--- testDUMP/otherTest.py
+++ testDUMP/otherTest.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-import time
+import os, errno, time
 
-time.sleep(10)
+FIFO1='mypipe1'
+
+with open(FIFO1, 'r') as fifo1:
+    print("OthrTst: FIFO1 opened")
+    time.sleep(10)
