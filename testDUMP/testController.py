1f7f40cfc1a780e4b7262ae2835ad9a13964ca57
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-06 15:03:20 -0400

    Update testController.py

--- testDUMP/testController.py
+++ testDUMP/testController.py
@@ -18,34 +18,90 @@ except OSError as oe:
         raise
 
 def killProcess():
+    print("5: got to killProcess def")
     counter=0
     while(True):
-        time.sleep(counter)
+        print("5: counter = "+str(counter))
+        time.sleep(1)
+        
         if(counter>=5):
-            with open(FIFO2, 'r') as fifo2:
-                print("KP1: FIFO1 opened")
-                while True:
-                    data = fifo1.read()
-                    print('KP1: process timed out--no data read')
+            with open(FIFO1, 'r') as fifo1:
+                print("5.1: KP1: FIFO1 opened")
+                data = fifo1.read()
+                print('5.2: KP1: process timed out--no data read')
             p1.kill()
             break
         else:
             counter+=1
-
+        
         if stop_broadcast.is_set():
             break
 
-
+#main
+print("1: got to MAIN def")            
 with open(FIFO1, 'w') as fifo1:
-    print("TSTCONT: FIFO1 opened")
+    print("2: TSTCONT: FIFO1 opened")
     
     watchdog1=Thread(target=killProcess)
     
+    print("3: watchdog started")
     watchdog1.start()
-    watchdog1.join(timeout=10)
-    fifo1.write("cmd " + str(rounds))
+
+    print("4: watchdog joined")
+    #watchdog1.join(timeout=10)
+    
+    print("6: fifo1 written to")
+    fifo1.write("Hello! ")
+    
+    print("7: broadcast stopped(?)")
     stop_broadcast.set()
     
-    print("TSTCONT: Message either sent or was killed")
-
+    print("8: TSTCONT: Message either sent or was killed")
 p1.kill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
