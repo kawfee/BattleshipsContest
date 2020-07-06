174dd33ed68f9ab4ce0397382133bb0aeaceda12
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-06 15:04:33 -0400

    Update ai1.py

--- ai1.py
+++ ai1.py
@@ -3,28 +3,21 @@
 import os
 import errno
 
-FIFO1 = 'mypipe1'
-
-try:
-	os.mkfifo(FIFO1)
-except OSError as oe:
-	if oe.errno != errno.EEXIST:
-		raise
-
+FIFO1CTA = 'cta1'
+FIFO1ATC = 'atc1'
 
 while ( True ):
-    with open(FIFO1, 'r') as fifo1:
-        print("AI1: FIFO1 opened")
+    with open(FIFO1CTA, 'r') as fifo1cta:
+        print("AI1: FIFO1CTA opened")
         while True:
-            data = fifo1.read()
+            data = fifo1cta.read()
             if len(data) == 0:
                 print("AI1: Writer closed")
                 break
             print('AI1: Read: "{0}"'.format(data))
 
 
-    with open(FIFO1, 'w') as fifo1:
-        print("AI1: FIFO1 opened")
-        fifo1.write("My name is Ai1")
+    with open(FIFO1ATC, 'w') as fifo1atc:
+        print("AI1: FIFO1ATC opened")
+        fifo1atc.write("My name is Ai1")
         print("AI1: Message sent")
-print("Never got here")
