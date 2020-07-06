e13bcbe1fc01cf166e4e6224aaa0acf16ab0dcc2
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-06 15:04:56 -0400

    Update ai2.py

--- ai2.py
+++ ai2.py
@@ -3,26 +3,21 @@
 import os
 import errno
 
-FIFO2 = 'mypipe2'
-
-try:
-	os.mkfifo(FIFO2)
-except OSError as oe:
-	if oe.errno != errno.EEXIST:
-		raise
-
+FIFO2CTA = 'cta2'
+FIFO2ATC = 'atc2'
 
 while ( True ):
-	with open(FIFO2, 'r') as fifo2:
-		print("AI2: FIFO2 opened")
-		while True:
-			data = fifo2.read()
-			if len(data) == 0:
-				print("AI2: Writer closed")
-				break
-			print('AI2: Read: "{0}"'.format(data))
+    with open(FIFO2CTA, 'r') as fifo2cta:
+        print("AI2: FIFO2CTA opened")
+        while True:
+            data = fifo2cta.read()
+            if len(data) == 0:
+                print("AI2: Writer closed")
+                break
+            print('AI2: Read: "{0}"'.format(data))
 
-	with open(FIFO2, 'w') as fifo2:
-		print("AI2: FIFO2 opened")
-		fifo2.write("My name is Ai2")
-		print("AI2: Message sent")
+
+    with open(FIFO2ATC, 'w') as fifo2atc:
+        print("AI2: FIFO2ATC opened")
+        fifo2atc.write("My name is Ai2")
+        print("AI2: Message sent")
