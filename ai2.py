761c627cee9a730608265c1d2eb28e638f4511a4
BouncingJoey <64807660+BouncingJoey@users.noreply.github.com>
2020-07-07 14:58:56 -0400

    Update ai2.py

--- ai2.py
+++ ai2.py
@@ -9,12 +9,8 @@ FIFO2ATC = 'atc2'
 while ( True ):
     with open(FIFO2CTA, 'r') as fifo2cta:
         print("AI2: FIFO2CTA opened")
-        while True:
-            data = fifo2cta.read()
-            if len(data) == 0:
-                print("AI2: Writer closed")
-                break
-            print('AI2: Read: "{0}"'.format(data))
+        data = fifo2cta.read()
+        print('AI2: Read: "{0}"'.format(data))
 
 
     with open(FIFO2ATC, 'w') as fifo2atc:
