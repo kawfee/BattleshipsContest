190c0542918c9ff9e574bc52e17880bd10dd9d01
Joey <mejosephg@gmail.com>
2020-12-18 17:11:07 -0500

    added a client_auto.cpp file to compile a fresh client_auto every time. we are blessed this day.

---
+++ client_auto.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <fstream>
+#include <cstring>
+
+#include "json.hpp"
+#include "defines.h"
+
+using namespace std;
+using json=nlohmann::json;
+
+void messageHandler(json &msg, string &clientID, int &round, char (&shipBoard)[10][10], char (&shotBoard)[10][10], int boardSize);
+void updateBoard(char board[10][10], int row, int col, int length, Direction dir, char newChar);
+void placeShip(json &msg, char shipBoard[10][10], int boardSize);
+void shootShot(json &msg, char shotBoard[10][10], int boardSize);
+void shotReturned(json &msg);
+void wipeBoards(char (&shipBoard)[10][10], char (&shotBoard)[10][10], int boardSize);
+void sendGameVars(json &msg);
+
+
+int main(int argc, char *argv[]){
+    srand(getpid());
+    string clientID = to_string(getpid());
+
+    const char *serverIp = "localhost"; int port = 54321;
+
+    //create a message buffer
+    char buffer[1500];
+
+    //setup a socket and connection tools
+    struct hostent* host = gethostbyname(serverIp);
+    sockaddr_in sendSockAddr;
+
+    bzero((char*)&sendSockAddr, sizeof(sendSockAddr));
+
+    sendSockAddr.sin_family = AF_INET;
+    sendSockAddr.sin_addr.s_addr =
+        inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    sendSockAddr.sin_port = htons(port);
+
+    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //try to connect...
+    int status = connect(clientSd,
+                         (sockaddr*) &sendSockAddr, sizeof(sendSockAddr));
+    if(status < 0){
+        cerr<<"Error connecting client to socket!"<<endl;
+    }else{
+        cout << "Connected client to the server!" << endl;
+    }
+
+    //declare variables for during the game
+    int round=0;
+    int boardSize=10;
+    char shipBoard[10][10];
+    char shotBoard[10][10];
+
+    //populate boards
+    wipeBoards(shipBoard, shotBoard, boardSize);
+    
+    while(1){
+        round++;
+
+        //read
+        memset(&buffer, 0, sizeof(buffer));//clear the buffer
+
+        recv(clientSd, (char*)&buffer, sizeof(buffer), 0);
+
+        string tempStr="";
+        tempStr.append(buffer);
+        //strcpy(tempStr, buffer);
+
+        json msg=json::parse(tempStr);
+
+        messageHandler(msg, clientID, round, shipBoard, shotBoard, boardSize);
+
+        memset(&buffer, 0, sizeof(buffer));//clear the buffer
+
+        if( msg.dump().length()+1 > 1500 ){
+            cerr << "UNSUPPORTED LENGTH REACHED BY: msg" << endl;
+        }
+        strcpy(buffer, msg.dump().c_str());
+        send(clientSd, (char*)&buffer, strlen(buffer), 0);
+    }
+
+    close(clientSd);
+    cerr << "Connection closed for client " << clientID << endl;
+
+    return 0;
+}
+
+void messageHandler(json &msg, string &clientID, int &round, char (&shipBoard)[10][10], char (&shotBoard)[10][10], int boardSize){
+    
+    if(msg.at("messageType")=="setupGame"){
+        sendGameVars(msg);
+    }else if(msg.at("messageType")=="matchOver"){
+        wipeBoards(shipBoard, shotBoard, boardSize);
+    }else if(msg.at("messageType")=="placeShip"){
+        msg.at("client") = clientID;
+        msg.at("count") = round;
+        placeShip(msg, shipBoard, boardSize);
+    }else if(msg.at("messageType")=="shootShot"){
+        msg.at("client") = clientID;
+        msg.at("count") = round;
+        shootShot(msg, shotBoard, boardSize);
+    }else if (msg.at("messageType")=="shotReturn"){
+        shotReturned(msg);
+    }else if(msg.at("messageType")=="shipDied"){
+        //char board[10][10], int row, int col, int length, Direction dir, char newChar
+        updateBoard(shipBoard, msg.at("row"), msg.at("col"), msg.at("length"), msg.at("dir"), KILL);
+    }else if (msg.at("messageType")=="killedShip"){
+        updateBoard(shotBoard, msg.at("row"), msg.at("col"), msg.at("length"), msg.at("dir"), KILL);
+    }
+    
+}
+
+void wipeBoards(char (&shipBoard)[10][10], char (&shotBoard)[10][10], int boardSize){
+    for(int row=0;row<boardSize;row++){
+        for(int col=0;col<boardSize;col++){
+            shipBoard[row][col]=WATER;
+            shotBoard[row][col]=WATER;
+        }
+    }
+}
+
+void placeShip(json &msg, char shipBoard[10][10], int boardSize){
+    for(int row=0;row<boardSize;row++){
+        for(int col=0;col<boardSize;col++){
+            if(shipBoard[row][col]==WATER){
+                msg.at("row") = row;
+                msg.at("col") = col;
+                msg.at("dir") = HORIZONTAL;
+                for(int len=0;len<msg.at("length");len++){
+                    if(shipBoard[row][col]!=WATER){
+                        msg.at("dir") = VERTICAL;
+                        updateBoard(shipBoard, row, col, msg.at("length"), VERTICAL, SHIP);
+                        return;
+                    }
+                }
+                updateBoard(shipBoard, row, col, msg.at("length"), HORIZONTAL, SHIP);
+                return;
+            }
+        }
+    }
+}
+
+void shootShot(json &msg, char shotBoard[10][10], int boardSize){
+    for(int row=0;row<boardSize;row++){
+        for(int col=0;col<boardSize;col++){
+            if(shotBoard[row][col]==WATER){
+                msg.at("row") = row;
+                msg.at("col") = col;
+                updateBoard(shotBoard, row, col, 1, NONE, SHOT);
+                return;
+            }
+        }
+    }
+}
+
+void updateBoard(char board[10][10], int row, int col, int length, Direction dir, char newChar){
+    if(dir==HORIZONTAL){
+        for(int len=0;len<length;len++){
+            board[row][col+len]=newChar;
+        }
+    }else if(dir==VERTICAL){
+        for(int len=0;len<length;len++){
+            board[row+len][col]=newChar;
+        }
+    }else if(dir==NONE){
+        board[row][col]=newChar;
+    }
+}
+
+void shotReturned(json &msg){
+    //cout << "Got to shotReturned() function in client" << endl;
+    // Do something with the message data here. 
+}
+
+void sendGameVars(json &msg){
+    msg.at("str") = "Matthew Bouch and Joey Gorski"; // Your author name(s) here
+}
+
--- makefile
+++ makefile
@@ -1,17 +1,18 @@
 CXXFLAGS= -std=c++11 -Wall
-ALLOBJECTS= client client_manual controller
+ALLOBJECTS= client client_auto client_manual controller
 
 # is this /* working? 
-object_files: client client_manual controller
+object_files: client client_auto client_manual controller
 	chmod 755 ./AI_Executables/*
 	chmod 755 controller
 client: client.cpp
+client_auto: client_auto.cpp
 client_manual: client_manual.cpp
 controller: controller.cpp
 
 # How to deal with making executables in sub-directory for a cleaner file system
 move:
-	make; mv client AI_Executables; mv client_manual AI_Executables;
+	make; mv client AI_Executables; mv client_auto AI_Executables; mv client_manual AI_Executables;
 
 clean:
-	rm -f ./AI_Executables/client ./AI_Executables/client_manual controller ais.txt
+	rm -f ./AI_Executables/client ./AI_Executables/client_auto ./AI_Executables/client_manual controller ais.txt
